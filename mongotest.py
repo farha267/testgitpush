1500ab161d391f866ec80a76acf6d86d8df120ca
farha <farhasultana267@gmail.com>
2022-07-18 20:30:25 +0530

    this is my frst code for push

---
+++ mongotest.py
@@ -0,0 +1,12 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://farha:12345@farha.kwooc.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+    "name": "farha",
+    "email": "farha@ineuron",
+    "surname": "sultana"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
